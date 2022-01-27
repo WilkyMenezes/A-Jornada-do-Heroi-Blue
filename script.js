bf8394735c51f6b638b3a2054cfa1a0fcfd2efa7
WilkyMenezes <willkymenezes@outlook.com>
2022-01-27 14:25:55 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello Word")
+var prompt = require('prompt-sync')();
+
+
+
+///Uma guerreira que  percorreu um longo caminho para enfrentar um bruxo e salvar seu povo.
+/// A guerrei possui duas espadas uma anti magia e a outra para ataques frontais.
+
+
+console.log('Digite: 1 para sim ou Digite: 2 para não')
+        perg1 = prompt("Pergunta 1: Diante do bruxo, Guerreira joga sua primeira espada anti magia para bloquear energia vital sendo sugado do povo: ");
+        perg2 = prompt("Pergunta 2: Guerreira com sua segunda espada, corta o braço direito do bruxo: ");
+        perg3 = prompt("Pergunta 3: O bruxo com suas habilidades joga um feitiço capaz de congelar, a guerreira pode esquivar: ");
+        perg4 = prompt("Pergunta 4: Bruxo se enfurece e ganha super velocidade, chegando o por traz, Porém a guerreira é capaz de perceber, ela o golpeia com seu giro junto a segunda espada: ");
+        perg5 = prompt("Pergunta 5: Bruxo libera um poder das sombras capaz de prender, mais a guerreira com sua primeira espada anti magia consegue bloqueiar o ataque: ");
+
+
+
+
+
